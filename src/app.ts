dabf1ee49444830a69660345c68c5b8bd87deabf
Tarikul Islam <tarikul3639@gmail.com>
2026-07-07 03:48:31 +0600

    feat: routes order reordering

--- src/app.ts
+++ src/app.ts
@@ -2,36 +2,32 @@ import express, { Application } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 
 import routes from "./routes";
+import { swaggerSpec } from "./config/swagger";
 
 import notFoundHandler from "./middlewares/notFound.middleware";
 import globalErrorHandler from "./middlewares/error.middleware";
 
-import swaggerUi from "swagger-ui-express";
-import { swaggerSpec } from "./config/swagger";
-
 const app: Application = express();
 
 app.use(helmet());
-
 app.use(cors());
-
 app.use(morgan("dev"));
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use(routes);
 
 // Swagger
 app.use(
     "/api-docs",
-    swaggerUi.serveFiles(swaggerSpec),
+    swaggerUi.serve,
     swaggerUi.setup(swaggerSpec)
 );
+
+// API Routes
+app.use(routes);
 
 // 404
 app.use(notFoundHandler);
